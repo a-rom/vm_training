509abe77984828da99e7f50684fd5081e3117049
root <root@FLH9Aai085.tky.mesh.ad.jp>
2016-11-04 22:38:10 +0900

    draft of install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#centos7のインストール			
+
+virt-install \
+--name=$1 \
+--vcpus=1 \
+--ram=1024 \
+--location=/tmp/CentOS-7-x86_64-Minimal-1511.iso \
+--disk path=/var/lib//libvirt/images/$1.img,format=qcow2,size=8 \
+--network bridge=virbr0 \
+--arch=x86_64 \
+--os-type=linux \
+--os-variant=rhel7
+--graphics none \
+--extra-args='console=tty0 console=ttyS0,115200n8 keymap=ja'
+exit 0
