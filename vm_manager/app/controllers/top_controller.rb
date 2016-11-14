15da1f9606adf22c8de94f2d2676f5954c3fee06
a-rom <a1623at@aiit.ac.jp>
2016-11-14 16:33:47 +0900

    railsからで既存のシェルスクリプトをすべて実行出来るようにしました

--- vm_manager/app/controllers/top_controller.rb
+++ vm_manager/app/controllers/top_controller.rb
@@ -1,14 +1,57 @@
 class TopController < ApplicationController
 
   def index
-  @records = Vm.all  
+  @records = Vm.all
   end
   
   def create
   end
 
   def create_vm
-   %x(sudo sh app/controllers/install.sh #{params[:vmname]})
+#id1から順に検索して、nameがNULLのレコードが見つかり且つid1から5までのcpuカラムと新しく入力されるcpuの値を足し合わせて、物理サーバーの上限cpuを超えない場合
    Vm.create(vmname: params[:vmname],status: "running")
+
+   %x(sudo sh app/controllers/install.sh #{params[:vmname]})
   end
+
+  def destroy
+  end
+
+  def destroy_vm
+#vmnameとparams[:vmname]}が同じ値だった場合に実行
+%x(sudo sh app/controllers/destroying.sh #{params[:vmname]})
+  end
+
+  def pending
+  end
+
+  def pending_vm
+#vmnameとparams[:vmname]}が同じ値だった場合に実行
+%x(sudo sh app/controllers/pending.sh #{params[:vmname]})
+  end 
+
+
+  def rebooting
+  end
+
+  def rebooting_vm
+  p 'pwd'
+  %x(sh app/controllers/rebooting.sh params[:vmname])
+  end
+
+  def resuming
+  end
+
+  def resuming_vm
+  p 'pwd'
+  %x(sh app/controllers/resuming.sh params[:vmname])
+  end
+  def starting
+  end
+
+  def starting_vm
+  p 'pwd'
+  %x(sh app/controllers/starting.sh params[:vmname])
+  end
+
 end
--- vm_manager/config/routes.rb
+++ vm_manager/config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #管理しているVMの一覧表示
   get '/' => 'top#index'
   get '/create' => 'top#create'
   post '/create_vm' => 'top#create_vm'
+  get '/pending' => 'top#pending'
+  post '/pending_vm' => 'top#pending_vm'
+  get '/rebooting' => 'top#rebooting'
+  post '/rebooting_vm' => 'top#rebooting_vm'
+  get '/resuming' => 'top#resuming'
+  post '/resuming_vm' => 'top#resuming_vm'
+  get '/destroy' => 'top#destroy'
+  post '/destroy_vm' => 'top#destroy_vm'
+  get '/starting' => 'top#starting'
+  post '/starting_vm' => 'top#starting_vm'
+
 end
